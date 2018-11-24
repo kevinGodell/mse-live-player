7860b5d3b915b652360bfe4292414ecf30d5f49b
Kevin Godell <kevin.godell@gmail.com>
2018-11-24 00:27:23 -0600

    adding dynamic routes to support additional streaming options

--- server.js
+++ server.js
@@ -2,11 +2,21 @@
 
 'use strict'
 
-const app = require('express')()
+const { Writable } = require('stream')
+
+const express = require('express')
+
+const app = express()
+
+app.disable('x-powered-by')
 
 const http = require('http').Server(app)
 
 const io = require('socket.io')(http/*, {origins: allowedOrigins} */)
+
+io.on('connect', (data) => {
+  console.log('io connect', data)
+})
 
 const Mp4Frag = require('mp4frag')
 
@@ -17,6 +27,7 @@ const P2J = require('pipe2jpeg')
 const ffmpegPath = require('ffmpeg-static').path
 
 app.all('/*', function (req, res, next) {
+  // todo verify authenticated and reroute if necessary
   console.log('req.url', req.url)
   next()
 })
@@ -228,13 +239,198 @@ app.get('/public/player.css', (req, res) => {
   res.sendFile(playerCss)
 })
 
-app.get('/:id/jpeg', (req, res) => {
+app.get('/:id/*', (req, res, next) => {
+  const id = req.params.id
+  // todo verify that id exists and that user has privilege
+  console.log('/:id/*', id)
+  const stream = streams[req.params.id]
+  if (!stream) {
+    res.status(404).end(`stream "${id}" not found`)
+    return
+  }
+  res.locals.id = id
+  res.locals.stream = stream
+  console.log('found stream', id)
+  next()
+})
+
+app.get('/:id/image.jpeg', (req, res) => {
+  const pipe2jpeg = res.locals.stream.pipe2jpeg
+  if (!pipe2jpeg) {
+    res.status(404)
+      .end(`image.jpeg not found for stream "${res.locals.id}"`)
+    return
+  }
+  const jpeg = pipe2jpeg.jpeg
+  if (!jpeg) {
+    res.status(503)
+      .set('Retry-After', 1)
+      .end(`image.jpeg not ready for stream "${res.locals.id}"`)
+    return
+  }
+  res.status(200)
+    .set('Content-Type', 'image/jpeg')
+    .end(jpeg)
+})
+
+app.get('/:id/video.mjpeg', (req, res) => {
+  const pipe2jpeg = res.locals.stream.pipe2jpeg
+  if (!pipe2jpeg) {
+    res.status(404)
+      .end(`video.mjpeg not found for stream "${res.locals.id}"`)
+    return
+  }
+  const jpeg = pipe2jpeg.jpeg
+  if (jpeg) {
+    send(res, jpeg)
+  } else {
+    // todo listen for jpeg event to make sure it starts pushing out jpegs, otherwise time out the connection
+    // timer will cancel request and probably give 503
+    pipe2jpeg.on('jpeg', (data) => {
+      send(res, data)
+    })
+  }
+
+  function send (res, jpeg) {
+    const writable = new Writable({
+      write (chunk, encoding, callback) {
+        res.write(`Content-Type: image/jpeg\r\nContent-Length: ${chunk.length}\r\n\r\n`)
+        res.write(chunk)
+        res.write('\r\n--cctv\r\n')
+        callback()
+      }
+    })
+    res.status(200)
+      .set('Content-Type', 'multipart/x-mixed-replace;boundary=cctv')
+      .write('--cctv\r\n')
+    writable.write(jpeg, { end: true })
+    pipe2jpeg.pipe(writable)
+    res.once('close', () => {
+      if (pipe2jpeg && writable) {
+        pipe2jpeg.unpipe(writable)
+      }
+      res.end()
+    })
+  }
+})
+
+app.get(`/:id/video.mp4`, (req, res) => {
+  const mp4frag = res.locals.stream.mp4frag
+  if (!mp4frag) {
+    res.status(404)
+      .end(`video.mp4 not found for stream "${res.locals.id}"`)
+    return
+  }
+  const init = mp4frag.initialization
+  if (init) {
+    send(res, init)
+  } else {
+    // todo set timer to cancel listener because initialized may never get called
+    // timer will cancel request and probably give 503
+    mp4frag.once('initialized', (data) => {
+      send(res, data.initialization)
+    })
+  }
+
+  function send (res, init) {
+    res.status(200)
+      .set('Content-Type', 'video/mp4')
+      .write(init)
+    if (mp4frag.segment) {
+      res.write(mp4frag.segment)
+    }
+    mp4frag.pipe(res, { end: true })
+    res.once('close', () => {
+      if (mp4frag) {
+        mp4frag.unpipe(res)
+      }
+      res.end()
+    })
+  }
+})
+
+app.get(`/:id/playlist.m3u8`, (req, res) => {
+  const mp4frag = res.locals.stream.mp4frag
+  if (!mp4frag) {
+    res.status(404)
+      .end(`playlist.m3u8 not found for stream "${res.locals.id}"`)
+    return
+  }
+  const m3u8 = mp4frag.m3u8
+  if (m3u8) {
+    send(res, m3u8)
+  } else {
+    // todo set timer to cancel listener because initialized may never get called
+    // timer will cancel request and probably give 503
+    mp4frag.once('initialized', (data) => {
+      send(res, mp4frag.m3u8)
+    })
+  }
+
+  function send (res, m3u8) {
+    res.status(200)
+      .set('Content-Type', 'application/vnd.apple.mpegurl')
+      .end(m3u8)
+  }
+})
+
+app.get(`/:id/playlist.m3u8.txt`, (req, res) => {
+  const mp4frag = res.locals.stream.mp4frag
+  if (!mp4frag) {
+    res.status(404)
+      .end(`playlist.m3u8.txt not found for stream "${res.locals.id}"`)
+    return
+  }
+  const m3u8 = mp4frag.m3u8
+  if (m3u8) {
+    send(res, m3u8)
+  } else {
+    // todo set timer to cancel listener because initialized may never get called
+    // timer will cancel request and probably give 503
+    mp4frag.once('initialized', (data) => {
+      send(res, mp4frag.m3u8)
+    })
+  }
+
+  function send (res, m3u8) {
+    res.status(200)
+      .set('Content-Type', 'text/plain')
+      .end(m3u8)
+  }
+})
+
+app.get('/:id/start', (req, res) => {
+  // todo
   try {
-    const jpeg = streams[req.params.id].pipe2jpeg.jpeg
-    res.writeHead(200, { 'Content-Type': 'image/jpeg' })
-    res.end(jpeg)
+    const ffmpeg = streams[req.params.id].ffmpeg
+    if (ffmpeg.running === true) {
+      console.log('ffmpeg already running')
+      ffmpeg.start()
+    } else {
+      console.log('starting ffmpeg')
+      ffmpeg.start()
+    }
+    res.status(200).send(`ffmpeg running: ${ffmpeg.running}`)
   } catch (e) {
-    res.status(404).send('jpeg not available')
+    res.status(404).send('ffmpeg not available')
+    res.destroy()
+  }
+})
+
+app.get('/:id/stop', (req, res) => {
+  // todo
+  try {
+    const ffmpeg = streams[req.params.id].ffmpeg
+    if (ffmpeg.running === false) {
+      console.log('ffmpeg already stopped')
+      ffmpeg.stop()
+    } else {
+      console.log('stopping ffmpeg')
+      ffmpeg.stop()
+    }
+    res.status(200).send(`ffmpeg running: ${ffmpeg.running}`)
+  } catch (e) {
+    res.status(404).send('ffmpeg not available')
     res.destroy()
   }
 })
@@ -242,3 +438,5 @@ app.get('/:id/jpeg', (req, res) => {
 http.listen(80, () => {
   console.log('listening on localhost:80')
 })
+
+// todo listen to video player stop and alert all connected streams
